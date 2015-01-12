df301dea5aa5d174b1b89fa584f13f5121d94757
metzlar <metzlar@gmail.com>
2015-01-12 14:08:34 -0800

    Update ZVCA.m

--- Mumps/ZVCA.m
+++ Mumps/ZVCA.m
@@ -11,10 +11,10 @@ CREATEVISITWNOTE(PRV,DFN,DIA,CONTENT) ;
         S TITLE="1467",VLOC="11",VSIT="",VSTR="11;"_VDT_";I",SUPPRESS="0",NOASF="1"
         D MAKE^TIUSRVP(.NIEN,DFN,TITLE,VDT,VLOC,VSIT,.TIUX,VSTR,SUPPRESS,NOASF)  
         S TIUX("TEXT",1,0)=CONTENT
-				S TIUX("POV")=DIA
+	S TIUX("POV")=DIA
         S TIUX("HDR")="1"_U_"1"
-				S TIUY=""
-				D SETTEXT^TIUSRVPT(.TIUY,NIEN,.TIUX,SUPPRESS)
+	S TIUY=""
+	D SETTEXT^TIUSRVPT(.TIUY,NIEN,.TIUX,SUPPRESS)
         Q
 CREATEVISIT(DIA,DFN,PRV) ; create a visit and return its timestamp
         S VDT=""
@@ -28,9 +28,9 @@ CREATEVISIT(DIA,DFN,PRV) ; create a visit and return its timestamp
         S PCELIST(2)="VST"_U_"DT"_U_VDT
         S PCELIST(3)="VST"_U_"PT"_U_DFN
         S PCELIST(4)="VST"_U_"HL"_U_VLOC
-				S PCELIST(5)="VST"_U_"VC"_U_"I" ; A??
+	S PCELIST(5)="VST"_U_"VC"_U_"I" ; A??
         S PCELIST(6)="PRV"_U_PRV_U_U_U_U_"1"
-				S PCELIST(7)="POV+^"_DIA_"^^"_DIATEXT_"^1^"_PRV_"^0^^^1"
-				S PCELIST(8)="COM^1^@"
+	S PCELIST(7)="POV+^"_DIA_"^^"_DIATEXT_"^1^"_PRV_"^0^^^1"
+	S PCELIST(8)="COM^1^@"
         D SAVE^ORWPCE(.OK,.PCELIST,NIEN,VLOC)
         Q VDT
